34a9198919b2caad17886a91e74626288ecd4afb
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-23 20:03:09 +0300

    fix typo

--- src/components/dashboard/Welcome.jsx
+++ src/components/dashboard/Welcome.jsx
@@ -5,7 +5,7 @@ export default function Welcome() {
     <div>
       <h1>Welcome to our shop!</h1>
       <p>
-        Please exlpore <Link to="/proucts">our products</Link> or share this
+        Please exlpore <Link to="/products">our products</Link> or share this
         site with others.
       </p>
     </div>
