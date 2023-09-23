77da496a4c2eaf1d8fbbd8e4dd3f3a2997b77493
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-23 19:29:42 +0300

    add dummy products data

---
+++ src/data/products.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: "p1",
+    title: "React - Complete Guide (Course)",
+    description:
+      "A thorough introduction to all key and also more advanced React concepts you must know.",
+    price: 18.99,
+  },
+  {
+    id: "p2",
+    title: "Focus Glasses",
+    description:
+      "The absolute best glasses you can wear for concentration-intensive tasks!",
+    price: 28.29,
+  },
+  {
+    id: "p3",
+    title: "Red Carpet",
+    description:
+      "Perfect for showing your friends that you are a high performer who made it.",
+    price: 539.99,
+  },
+];
+
+export default products;
