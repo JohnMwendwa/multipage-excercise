461fe9b5dd699d1e14f78d3f7abc4c43af6f186e
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-23 20:44:19 +0300

    create different route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
+import WelcomePage from "./pages/Welcome";
+import Navbar from "./components/shared/Navbar";
+import ProductsPage from "./pages/Products";
+import ProductDetailsPage from "./pages/ProductDetails";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<ProductDetailsPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
