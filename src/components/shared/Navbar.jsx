106610f575bcf8b9d362c813b6557445b0be437a
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-23 19:55:23 +0300

    create the Navbar component

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <header>
+      <nav id="main-nav">
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={(isActive) => (isActive ? "active" : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/products"
+              className={(isActive) => (isActive ? "active" : "")}
+            >
+              Our Products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
