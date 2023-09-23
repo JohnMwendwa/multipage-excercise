35a835693f462825dbe3ef92460360cd4fd1d098
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-23 20:56:49 +0300

    wrap the whole app with the Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 
 import WelcomePage from "./pages/Welcome";
-import Navbar from "./components/shared/Navbar";
 import ProductsPage from "./pages/Products";
 import ProductDetailsPage from "./pages/ProductDetails";
+import Layout from "./components/shared/Layout";
 
 function App() {
   return (
-    <>
-      <Navbar />
+    <Layout>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/products/:id" element={<ProductDetailsPage />} />
       </Routes>
-    </>
+    </Layout>
   );
 }
 
