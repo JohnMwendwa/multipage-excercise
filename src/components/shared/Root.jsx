71dee3c386ae112b0b1c1f61b6ef98ec916ec0d0
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-23 21:27:03 +0300

    create Root component

--- src/components/shared/Root.jsx
+++ src/components/shared/Root.jsx
@@ -1,1 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Layout from "./Layout";
 
+export default function Root() {
+  return (
+    <>
+      <Layout />
+      <Outlet />
+    </>
+  );
+}
