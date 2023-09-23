b5b7b16079821de48cecb740f873b6a7d8cb2904
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-23 21:31:38 +0300

    group the route paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 
 import WelcomePage from "./pages/Welcome";
-import Layout from "./components/shared/Layout";
+import Root from "./components/shared/Root";
 
 const ProductsPage = lazy(() => import("./pages/Products"));
 const ProductDetailsPage = lazy(() => import("./pages/ProductDetails"));
+const NotFoundPage = lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
-    <Layout>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Routes>
-          <Route path="/" element={<WelcomePage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/products/:id" element={<ProductDetailsPage />} />
-        </Routes>
-      </Suspense>
-    </Layout>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route element={<WelcomePage />} index />
+          <Route path="products" element={<ProductsPage />} />
+          <Route path="products/:id" element={<ProductDetailsPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
